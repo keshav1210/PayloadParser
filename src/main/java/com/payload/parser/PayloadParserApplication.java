641c9f7239b41f29921f39a5df86be8db4abcd16
Your Name <you@example.com>
2026-01-04 19:39:48 +0530

    first commit

---
+++ src/main/java/com/payload/parser/PayloadParserApplication.java
@@ -0,0 +1,13 @@
+package com.payload.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayloadParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayloadParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payload/parser/PayloadParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payload.parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayloadParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
