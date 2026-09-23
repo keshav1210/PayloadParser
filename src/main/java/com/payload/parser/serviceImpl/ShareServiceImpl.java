7a258fa94064917b0a88b886387c78393fab8cff
Your Name <you@example.com>
2026-09-23 22:30:35 +0530

    file size

--- src/main/java/com/payload/parser/serviceImpl/ShareServiceImpl.java
+++ src/main/java/com/payload/parser/serviceImpl/ShareServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 
 @Service
 public class ShareServiceImpl implements ShareService {
-    private static final long MAX_FILE_SIZE = 30 * 1024* 1024; // 300 KB
+    private static final long MAX_FILE_SIZE = 40 * 1024* 1024; // 40
     @Autowired
     private Cache<String, ShareMeta> cache;
 
